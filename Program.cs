85862a93a626a4065b27241e405eb836acaee15b
Robert Bakyayita <rw-bakyayita@outlook.com>
2025-07-26 21:45:28 +0200

    build clean

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Numerics; // Ensure Complex is available
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<QuantumGate>(); // Register QuantumGate as a singleton service
+
+
 
 var app = builder.Build();
 
